9c12fe9f6672293ea830db993dc4b51d16abbc16
Daniel Corrales <dmcorrales@unbosque.edu.co>
2019-05-27 22:55:48 -0500

    Updates

---
+++ precache-manifest.ddb81d0ebcdd930bf7e5405769452842.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "32800f70032611d1d644",
+    "url": "/erp/static/css/main.218d3115.chunk.css"
+  },
+  {
+    "revision": "32800f70032611d1d644",
+    "url": "/erp/static/js/main.32800f70.chunk.js"
+  },
+  {
+    "revision": "27e4784bc19e50a046b5",
+    "url": "/erp/static/css/1.46c90f74.chunk.css"
+  },
+  {
+    "revision": "27e4784bc19e50a046b5",
+    "url": "/erp/static/js/1.27e4784b.chunk.js"
+  },
+  {
+    "revision": "622cdf5010be406c77ea",
+    "url": "/erp/static/js/runtime~main.622cdf50.js"
+  },
+  {
+    "revision": "437f2a22bfff86c969445337c144ed4e",
+    "url": "/erp/index.html"
+  }
+];
